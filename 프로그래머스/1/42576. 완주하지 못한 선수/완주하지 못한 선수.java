c85e6eda23938799f22c366ba9702e0e21f7b2c5
kimeunseokk <kes0740@daum.net>
2025-09-24 15:06:39 +0900

    Update 완주하지 못한 선수.java

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,7 +4,7 @@ class Solution {
         Map<String,Integer> map = new HashMap<>();
         String answer = "";
         for(String now : participant){
-            map.put(now , map.getOrDefault(now,0)+1);
+            map.put(now , map.getOrDefault(now,0)+1); // 키값에 getOrDefault(now,0)+1 --> now에 값이 없으면 키값의 기본값을 0ㅇ로 지정한다 
         }
         for(String p : completion){
             map.put(p, map.get(p)-1);
